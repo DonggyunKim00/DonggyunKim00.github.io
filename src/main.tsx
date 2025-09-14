3d773244cf14442470bd6eadcfc75b63ab82156d
donggyun <donggyunkim12@gmail.com>
2025-09-14 15:30:11 +0900

    feat: ThemeProvider 래핑

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './style/index.css';
+import App from './App.tsx';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const theme = createTheme({
+  typography: {
+    // h1: {
+    //   fontSize: '3rem',
+    //   fontWeight: 700,
+    // },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
