727f91e94e2939941a260c45c47b67552210718e
donggyun <donggyunkim12@gmail.com>
2025-09-14 17:04:16 +0900

    feat(Intro): 인트로 초기 UI 구현

---
+++ src/components/Intro.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+import { Typography } from '@mui/material';
+
+const Intro = () => {
+  return (
+    <Container>
+      <Image src="/donggyun.jpg" width={200} />
+      <Typography variant="h1" fontSize={34} fontWeight={700} color="#121212">
+        김동균
+      </Typography>
+
+      <Typography variant="h2" fontSize={20} fontWeight={400} color="#7e7e7e">
+        Frontend Developer
+      </Typography>
+    </Container>
+  );
+};
+
+export default Intro;
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  padding: 32px 16px;
+  border-top: 1px solid #e8e8e8;
+`;
+
+const Image = styled.img`
+  border-radius: 10px;
+  margin-bottom: 20px;
+`;
